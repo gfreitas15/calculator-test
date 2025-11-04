47cf310df87926148e7e89d9921115fed4512597
GFreitas15 <gfmonguilhott793@gmail.com>
2025-11-03 23:50:36 -0300

    RED: Add multiplication test function

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,4 +8,10 @@ class CalculatorTest {
         Calculator calc = new Calculator();
         assertEquals(8, calc.sum(5, 3));
     }
+
+    @Test
+    void testMult() {
+        Calculator calc = new Calculator();
+        assertEquals(12, calc.mult(6, 2));
+    }
 }
